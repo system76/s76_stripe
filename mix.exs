2c2b92d143fcd39af7ef281eaad991b725ec4b9e
Ben Cates <ben@system76.com>
2018-01-15 13:59:05 -0700

    Bump version to make Hex happy

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Stripe.Mixfile do
   def project do
     [
       app: :s76_stripe,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       description: description(),
