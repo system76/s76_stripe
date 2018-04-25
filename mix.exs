5c9962185089982ae59cff50c47a29dda3e772a0
Ben Cates <ben@system76.com>
2018-04-25 11:39:59 -0600

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Stripe.Mixfile do
   def project do
     [
       app: :s76_stripe,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       description: description(),
